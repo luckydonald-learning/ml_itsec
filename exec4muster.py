3183fcf2285e4190ada2d1a520d5fb5109d300fe
luckydonald <luckydonald@flutterb.at>
2018-05-31 17:52:22 +0200

    More cleanup. Imports this time.

--- exec4muster.py
+++ exec4muster.py
@@ -1,15 +1,31 @@
-import os
 from os import path
-import numpy
-from numpy.linalg import norm
+
+from exec3 import split_word, do_count, calc_k, merge_alphabets
+
+import math
+import os
+import re
+from collections import Counter
+
+# preprocessing
+import nltk
 from luckydonaldUtils.logger import logging
+
+
+# package related inits
+try:
+    from nltk.corpus import stopwords
+    stopwords.words('english')
+except LookupError:
+    nltk.download('stopwords')
+    from nltk.corpus import stopwords
+# end try
 
 logger = logging.getLogger(__name__)
 logging.add_colored_handler(level=logging.DEBUG, date_formatter='%Y-%m-%d %H:%M:%S')
 
 
-from exec3 import split_word, do_count, calc_k, merge_alphabets
-
+# settings
 
 FOLDER = path.join('..','..',"Machine Learning for Computer Security", "Exercises", "mlsec-exer04-data")
 
@@ -70,64 +86,7 @@ def get_words(folder, filename_list, delemiters=[' '], per_file=False, omit_word
 # end def
 
 
-def center_of_mass(alphabet, counts):
-    n = len(alphabet)
-    return (1 / n) * sum(counts[alphabet[i]] for i in range(0, n))
-# end def
 
-
-z_de = """
-Subject: Wir machen dich mit Schönheiten bekannt
-Ich habe erfahren , dass mein Mann fremdgeht!
-Nun will ihm mit gleicher Münze zahlen !
-Suchen nach einem netten Kerl für Sex .
-Hier sind meine Fotos und Profil ."""
-
-z_en = """
-Subject: EMMANUEL SAMS & ASSOCIATES
-EMMANUEL SAMS & ASSOCIATES
-SOLICITORS AND ADVOCATES
-214 / GRE APPAPA LAGOS NIGERIA.
-POST CODE 23401
-PRIVATE EMAIL : ( emmanuelsams 59 @ yahoo . com )
-
-Complements ! ! . firstly , I must apologize for barging into your mailbox without a formal introduction of myself to you .
-
-Actually , I got your contact information from a reputable business / professional directory of your country which gives me
-
-assurance of your legibility as a person while trying to get a good and capable business person in your country for both business and investment purposes . Let me start by introducing myself ; I am Barrister EMMANUEL SAMS   ( Esq . ) I was the Personal Attorney to late Mr . Morris Thompson an American who was a private businessman in my country unfortunately lost his life in the plane crash of Alaska Airlines Flight 261 which crashed on January 31 st 2000 , including his wife and only daughter .
-
-Before the plane crash , Mr . Morris Thompson made a deposit value of US $ 20 , 000 , 000 . 00 ( TWENTY MILLION UNITED STATE DOLLARS ) in the STANDARDTRUST BANK OF NIGERIA  Upon maturity several notice was sent to him , another notification was sent and still no response came from him . I later found out that Mr . Morris Thompson and his family had been killed in that plane crash .
-
-After further investigation it was also discovered that Mr . Morris Thompson ’ s next of kin was his daughter who died with him in the crash . What borders me most is that according to the laws of my country at the expiration of 8 years the funds will revert to the ownership of the NIGERIA GOVERNMENT , if nobody applies to claim the funds . Against this backdrop , my suggestion to you is that I will like you as a
-foreigner to stand as the next of kin to Mr . Morris Thompson so that you will be able to receive these funds for both of us . WHAT IS TO BE DONE ?
-
-I want you to know that I have had everything planned out so that we shall come out successfully . As a barrister , I will prepare the necessary document that will back you up as the next of kin to Mr . Morris Thompson . PLEASE YOU HAVE TO PROVIDE YOUR FULL INFORMATION SUCH AS STATED BELOW ,
-
-[ 1 ] NAME :
-[ 2 ] ADDRESS :
-[ 3 ] AGE :
-[ 4 ] SEX :
-[ 5 ] TELEPHONE :
-[ 6 ] FAX :
-[ 7 ] OCCUPATION STATUS :
-
-Also be informed that this Transaction will take us just 15 working days to accomplish beginning from when I receive your Data ’ s . After you have been made the next of kin , I will also file an application to the bank on your behalf as soon as I secure the necessary approval and letter of probate in your favor for the movement of the funds to an account that will be provided by you . This process is 100 % risk free as I have set out all the modalities to see that a legalized method is used because then I will prepare all the necessary documents .
-
-Please note that utmost secrecy and confidentiality is required at all times during this transaction .
-
-Once the funds have been transferred into your nominated bank account we shall share in the ratio of 55 % for me , 45 % for you . I will prefer that you reach me via this email address : ( emmanuelsams59 @ yahoo . com )
-
-Your earliest response to this offer will be appreciated
-
-Best Regards
-Barr . EMMANUEL SAMS"""
-
-
-import math
-import os
-import re
-from collections import Counter
 
 
 def bowk(x, z, d=1.0, normalize=True):
@@ -334,17 +293,7 @@ class Classifier(object):
     def _simple(self, message):
         return self._classic(message) + self._reverse(message)
     # end def
-
-
-### Prerocessing
-import nltk
-try:
-    from nltk.corpus import stopwords
-    stopwords.words('english')
-except LookupError:
-    nltk.download('stopwords')
-    from nltk.corpus import stopwords
-# end try
+# end class
 
 
 def word_filter(word):
