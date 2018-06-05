1235edb16a2dd8f292f36a8fce4e2ca064391e79
luckydonald <luckydonald@flutterb.at>
2018-06-05 14:37:32 +0200

    HARDCORE BRUTEFORCE LINE PAINTING

--- exec5.py
+++ exec5.py
@@ -52,10 +52,16 @@ f(row) = sign(⟨ϕ(row), w⟩) = sign(sum((x0, x1) * w)
        = sign(sum(feature * w for feature in [x0, x1])
 """
 
+
+def f(w, features):
+    return np.sign(sum(feature * w[_i] for _i, feature in enumerate(features)))  # prediction
+# end def
+
 LABELS = 0
 DATA = 1
 
 w = np.array([0.754645564, 0.087735])  # random, I hit my head on the keyboard.
+w = np.array([4.42, 2.3])  # random, I hit my head on the keyboard.
 # weights
 
 N = len(train_set[LABELS][0])
@@ -65,14 +71,14 @@ w1 = [w[1]]
 
 for i in range(N):
     x0, x1 = train_set[DATA][i]  # (x0, x1)
-    ϕ = np.array([x0, x1])  # feature vector
+    ϕ = np.array([x0, x1])  # feature
 
-    f = np.sign(sum(feature * w[_i] for _i, feature in enumerate([x0, x1])))  # prediction
+    prediction = f(w, ϕ)
 
-    yi = train_set[LABELS][0][i]  # label
+    label = train_set[LABELS][0][i]  # label yi
 
-    if f != yi:  # label != prediction
-        w = w + yi * ϕ
+    if prediction != label:  # label != prediction
+        w = w + label * ϕ
     # end if
     w0.append(w[0])
     w1.append(w[1])
@@ -118,5 +124,28 @@ subplt.plot(neg['x'], neg['y'], 'r.', label='negative')
 
 subplt.plot([0, w[0]*2], [0, w[1]*-2], label='w')
 subplt.legend(loc="lower right")
+
+
+print('lel')
+layout = GridSpec(3,2)
+fig = plt.figure()
+subplt = fig.add_subplot(layout[1:, :])
+pos = {'x': [], 'y': []}
+neg = {'x': [], 'y': []}
+i = 0
+for x in range(0, 70):
+    for y in range(0, 70):
+        if f(w, [x,y]) == -1:
+            neg['x'].append(x*0.1)
+            neg['y'].append(y*0.1)
+        else:
+            pos['x'].append(x*0.1)
+            pos['y'].append(y*0.1)
+        # end if
+    # end for
+# end for
+subplt.plot(pos['x'], pos['y'], 'g.', label='postive')
+subplt.plot(neg['x'], neg['y'], 'r.', label='negative')
+
 
 plt.show()
